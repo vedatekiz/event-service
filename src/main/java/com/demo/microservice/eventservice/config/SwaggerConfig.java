0fe6535a8f72a5647946c90343d3766048e7c0d9
Ekiz Ali Vedat z003jsxt <vedat.ekiz@siemens.com>
2018-07-13 11:56:41 +0300

    Swagger support added

---
+++ src/main/java/com/demo/microservice/eventservice/config/SwaggerConfig.java
@@ -0,0 +1,55 @@
+package com.demo.microservice.eventservice.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Optional;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error")))
+                .build()
+                .apiInfo(metadata())
+                .useDefaultResponseMessages(false)
+                .genericModelSubstitutes(Optional.class);
+
+    }
+
+    private ApiInfo metadata() {
+        return new ApiInfoBuilder()
+                .title("Event service API")
+                .description(
+                        "This is a rabbit mq listener event service ")
+                .version("1.0.0")
+                .contact(new Contact(null, null, "vedat.ekiz@gmail.com"))
+                .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
